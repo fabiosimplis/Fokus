4e421c237df6208d6ba3e69ca597375a3ea31696
fabiosimplis <fabio.simplis@gmail.com>
2025-01-06 15:21:33 -0300

    add desselecionando uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -6,9 +6,8 @@ const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 
-
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
-
+let tarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -53,6 +52,16 @@ function criarElementoTarefa(tarefa) {
     li.append(botao);
 
     li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+        .forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active');
+        });
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return;
+        }
+        tarefaSelecionada = tarefa;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
         li.classList.add('app__section-task-list-item-active');
     }
