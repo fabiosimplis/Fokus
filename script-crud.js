3fd9245701f3b336fcccb987003d88472fc035d2
fabiosimplis <fabio.simplis@gmail.com>
2025-01-06 09:39:36 -0300

    add leitura de tarefas gravadas

--- script-crud.js
+++ script-crud.js
@@ -3,8 +3,9 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
@@ -23,12 +24,14 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button');
     const imagemBotao = document.createElement('img');
-    imagemBotao.setAttribute('src', '/imagens/edit.png');
+    imagemBotao.setAttribute('src', './imagens/edit.png');
     botao.append(imagemBotao);
 
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    return li;
 }
 
 btnAdicionarTarefa.addEventListener('click', () => {
@@ -42,4 +45,9 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     }
     tarefas.push(tarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
 });
